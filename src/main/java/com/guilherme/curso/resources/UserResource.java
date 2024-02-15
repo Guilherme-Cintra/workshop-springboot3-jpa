c3e7445bacdc69c510ea7373e4bee81a89dcbc7d
guima <guilherme101198@icloud.com>
2024-02-14 19:47:12 -0500

    User entity and resource

---
+++ src/main/java/com/guilherme/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.guilherme.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "MariaAmor1233@gmail.com", "9999999", "mariatima");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
