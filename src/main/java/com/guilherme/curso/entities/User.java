9f7f29bea2826724ef2d80658a924e97894973a6
guima <guilherme101198@icloud.com>
2024-02-15 00:02:00 -0500

    JPA repository, dependency injection, database seeding

--- src/main/java/com/guilherme/curso/entities/User.java
+++ src/main/java/com/guilherme/curso/entities/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	
 	private String name;
 	
@@ -32,7 +32,7 @@ public class User implements Serializable{
 		
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -45,7 +45,7 @@ public class User implements Serializable{
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
