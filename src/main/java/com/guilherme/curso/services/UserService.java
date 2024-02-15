723219f7fdfd290256905ef21e77e00ecbbdbdbc
guima <guilherme101198@icloud.com>
2024-02-15 10:46:37 -0500

    service layer, component registration

---
+++ src/main/java/com/guilherme/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.guilherme.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.curso.entities.User;
+import com.guilherme.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> objt = userRepository.findById(id);
+		return objt.get();
+	}
+}
