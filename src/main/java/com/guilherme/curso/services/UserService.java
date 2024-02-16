4334136287b540a7cb657b5908cb0ff70106aa38
guima <guilherme101198@icloud.com>
2024-02-15 19:25:18 -0500

    handling exceptions

--- src/main/java/com/guilherme/curso/services/UserService.java
+++ src/main/java/com/guilherme/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.curso.entities.User;
 import com.guilherme.curso.repositories.UserRepository;
+import com.guilherme.curso.services.exceptions.DatabaseException;
+import com.guilherme.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,12 +22,41 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	
+	
 	public User findById(Long id) {
-		Optional<User> objt = userRepository.findById(id);
-		return objt.get();
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+	
 	
 	public User insert(User obj) {
 		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		if (!userRepository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+		try {
+			userRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+	    }
+	
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 }
