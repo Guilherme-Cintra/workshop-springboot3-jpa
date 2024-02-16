cb75f8ee81596ad8a8b50bc34ff02a8e4b0c5be9
guima <guilherme101198@icloud.com>
2024-02-15 22:25:03 -0500

    update exception

--- src/main/java/com/guilherme/curso/services/UserService.java
+++ src/main/java/com/guilherme/curso/services/UserService.java
@@ -12,6 +12,8 @@ import com.guilherme.curso.repositories.UserRepository;
 import com.guilherme.curso.services.exceptions.DatabaseException;
 import com.guilherme.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -48,9 +50,16 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			// TODO: handle exception
+			throw new ResourceNotFoundException(id);
+		}
+		
+		
 	}
 
 	private void updateData(User entity, User obj) {
